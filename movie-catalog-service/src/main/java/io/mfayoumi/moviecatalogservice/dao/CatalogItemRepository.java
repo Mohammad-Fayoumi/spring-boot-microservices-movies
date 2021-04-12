15ac65dd96404ec4d428c429e0881e64ae06c453
Mohammad Fayoumi <mohammadfayoumi7@gmail.com>
2021-04-12 23:05:12 +0300

    remove un-used packages

--- movie-catalog-service/src/main/java/io/mfayoumi/moviecatalogservice/dao/CatalogItemRepository.java
+++ movie-catalog-service/src/main/java/io/mfayoumi/moviecatalogservice/dao/CatalogItemRepository.java
@@ -2,8 +2,6 @@ package io.mfayoumi.moviecatalogservice.dao;
 
 import io.mfayoumi.moviecatalogservice.entity.CatalogItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface CatalogItemRepository extends JpaRepository<CatalogItem, Integer> {
 }
