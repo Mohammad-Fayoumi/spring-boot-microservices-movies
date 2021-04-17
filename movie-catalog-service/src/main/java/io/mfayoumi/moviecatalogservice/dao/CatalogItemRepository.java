21a285049bb7ac5f9802dc868be3e064bf6534d2
Mohammad Fayoumi <mohammadfayoumi7@gmail.com>
2021-04-17 21:48:21 +0300

    Inject and use RestTemplate

--- movie-catalog-service/src/main/java/io/mfayoumi/moviecatalogservice/dao/CatalogItemRepository.java
+++
@@ -1,7 +0,0 @@
-package io.mfayoumi.moviecatalogservice.dao;
-
-import io.mfayoumi.moviecatalogservice.entity.CatalogItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogItemRepository extends JpaRepository<CatalogItem, Integer> {
-}
